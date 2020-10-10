8c548cb099c6d40d679720ad5df7ceee8a31ae48
NicolasMaxime <maxime1.nicolas@gmail.com>
2020-10-10 13:05:44 +0200

    adding javadoc as part of project report

--- src/main/java/org/apache/maven/demo/SimpleClass.java
+++ src/main/java/org/apache/maven/demo/SimpleClass.java
@@ -1,6 +1,12 @@
 package org.apache.maven.demo;
 
+/**
+ * Une simple classe pour jouer avec le project builder.
+ */
 public class SimpleClass {
+    /**
+     * Display "Simple class constructor"
+     */
     public SimpleClass(){
         System.out.println("SimpleCLass constructor");
     }
